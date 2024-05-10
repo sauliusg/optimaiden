f11bec6aacdfc43b8b49ca77e4191d309b7c5482
saulius <saulius@b9d307a2-75af-4218-822f-dfac11aef269>
2024-05-10 14:01:49 +0000

    Adding server configuration; now it does not fail on waiting closed sockets (do not know why...).
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/optimaiden/trunk@9 b9d307a2-75af-4218-822f-dfac11aef269

--- src/optimaiden.adb
+++ src/optimaiden.adb
@@ -1,11 +1,17 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Command_Line;
+
 with AWS.Server;
+with AWS.Config; use AWS.Config;
+with AWS.Config.Set;
+with AWS.Net;
 
 with Optimaiden_Callbacks; use Optimaiden_Callbacks;
 
 procedure Optimaiden is
 
    Web_Server : AWS.Server.HTTP;
-   
+
    task Waiter is
       entry Start;
       entry Wait;
@@ -21,17 +27,39 @@ procedure Optimaiden is
       end select;
    end Waiter;
 
+   Web_Config : AWS.Config.Object := Get_Current;
+
 begin
+
+   declare
+      use AWS.Config.Set;
+   begin
+      Max_Connection (Web_Config, 1);
+      -- Free_Slots_Keep_Alive_Limit (Web_Config, 1);
+      -- Keep_Alive_Force_Limit(Web_Config, 1);
+   end;
+
+   AWS.Config.Set.Server_Name (Web_Config, "Optimaiden: Hello World");
+
+   Put_Line (AWS.Config.Free_Slots_Keep_Alive_Limit (Web_Config)'Image);
+   Put_Line (AWS.Config.Max_Connection (Web_Config)'Image);
 
    AWS.Server.Start
      (
-      Web_Server, "Hello World",
-      Max_Connection => 1,
-      Callback       => HW_CB'Access
+      Web_Server,
+      Callback => HW_CB'Access,
+      Config => Web_Config
      );
 
    Waiter.Wait;
 
    AWS.Server.Shutdown (Web_Server);
+
+exception
+   when AWS.Net.Socket_Error =>
+      Put_Line (Standard_Error, "Socket already in use");
+      Ada.Command_Line.Set_Exit_Status (1);
+      Waiter.Start;
+      Waiter.Wait;
 
 end Optimaiden;
--- src/optimaiden_callbacks.ads
+++ src/optimaiden_callbacks.ads
@@ -5,4 +5,4 @@ package  Optimaiden_Callbacks is
 
    function HW_CB (Request : AWS.Status.Data) return AWS.Response.Data;
    
-end;
+end Optimaiden_Callbacks;
