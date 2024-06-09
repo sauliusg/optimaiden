749fc8b1c2fca8798b013b839176920c6197dc3c
saulius <saulius@b9d307a2-75af-4218-822f-dfac11aef269>
2024-06-09 13:51:40 +0000

    Adding the external CIF Streaming Parser library from:
    Working Copy Root Path: /home/saulius/src/cif-streaming-parser
    URL: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/cif-streaming-parser/trunk
    Relative URL: ^/trunk
    Repository Root: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/cif-streaming-parser
    Repository UUID: 97ea2815-8876-4f85-8280-457256952e91
    Revision: 137
    Node Kind: directory
    Schedule: normal
    Last Changed Author: Mindaugas_Legeckas
    Last Changed Rev: 137
    Last Changed Date: 2023-05-31 10:43:49 +0300 (Wed, 31 May 2023)
    
    Ignoring backup files ("*~") in Git.
    
    trunk/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/optimaiden/trunk@29 b9d307a2-75af-4218-822f-dfac11aef269

---
+++ externals/cif-streaming-parser/r137/trunk/src/externals/cod-tools/src/components/codcif/.cifmessage.d
@@ -0,0 +1,44 @@
+./obj/cifmessage.o: cifmessage.c /usr/include/stdc-predef.h cifmessage.h \
+ ../../externals/cexceptions/cexceptions.h /usr/include/setjmp.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /home/saulius/install/gnat-alire/gnat-alire-13.2.1/lib/gcc/x86_64-pc-linux-gnu/13.2.0/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /home/saulius/install/gnat-alire/gnat-alire-13.2.1/lib/gcc/x86_64-pc-linux-gnu/13.2.0/include-fixed/x86_64-linux-gnu/bits/floatn.h \
+ /home/saulius/install/gnat-alire/gnat-alire-13.2.1/lib/gcc/x86_64-pc-linux-gnu/13.2.0/include-fixed/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ ../../externals/cexceptions/allocx.h \
+ ../../externals/cexceptions/stringx.h /usr/include/assert.h
