00f48cfc0fb2814a7b1503af94d30d5d195db55e
saulius <saulius@b9d307a2-75af-4218-822f-dfac11aef269>
2024-06-10 11:18:34 +0000

    Starting JSON output of the 'structures' endpoint.
    trunk/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/optimaiden/trunk@39 b9d307a2-75af-4218-822f-dfac11aef269

--- src/optimaiden_structure_handler.adb
+++ src/optimaiden_structure_handler.adb
@@ -1,15 +1,58 @@
 with AWS.MIME; use AWS.MIME;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Util.Streams.Texts;
+with Util.Serialize.IO.JSON;
+with Cif_Streaming_Parser; use Cif_Streaming_Parser;
+with Cif_Datablock; use Cif_Datablock;
 
 package body Optimaiden_Structure_Handler is
-
+   
+   CIF_File_Name : String := "tests/data/2000000.cif";
+   
+   procedure Write
+     (
+      Stream : out Util.Serialize.IO.JSON.Output_Stream;
+      CIF_Datablock : Controlled_Datablock_Access
+     ) is
+   begin
+      Stream.Start_Document;
+      Stream.Start_Array ("data");
+      
+      Stream.Start_Entity ("");
+      Stream.Start_Entity ("attributes");
+      
+      Stream.Write_Entity ("id", Get_Datablock_Name (CIF_Datablock));
+      
+      Stream.End_Entity ("attributes");
+      Stream.Write_Entity ("type", "structures");
+      Stream.End_Entity ("");
+      
+      Stream.End_Array ("data");
+      Stream.End_Document;
+   end;
+   
    overriding
    function Dispatch
      (
       Handler : Optimaiden_Structure_Handler_Type;
       Request : AWS.Status.Data
      ) return AWS.Response.Data is
+      Cif_Datablock :  Controlled_Datablock_Access;
+      Output : aliased Util.Streams.Texts.Print_Stream;
+      Stream : Util.Serialize.IO.JSON.Output_Stream;
    begin
-      return AWS.Response.Build (AWS.MIME.Text_Plain, "structures will go here");
+      Parse_Cif_From_File (To_Unbounded_String (CIF_File_Name));
+      Dequeue_Datablock (Cif_Datablock);
+      
+      Output.Initialize (Size => 10000);
+      Stream.Initialize (Output => Output'Unchecked_Access);
+      Write (Stream, Cif_Datablock);
+
+      return AWS.Response.Build
+        (
+         AWS.MIME.Application_JSON,
+         Util.Streams.Texts.To_String (Output)
+        );
    end Dispatch;
 
    overriding function Clone (Element : Optimaiden_Structure_Handler_Type)
