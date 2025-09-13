5d55aef317ecb66a3d2c202958883f03031e3d8b
saulius <saulius@b9d307a2-75af-4218-822f-dfac11aef269>
2025-09-13 17:09:49 +0000

    Successfully generating the grammar, albeit with a large number of conflicts:
    ayacc -k filter.y
     142 Shift/Reduce Conflicts
     50 Reduce/Reduce Conflicts
    
    branches/saulius-parse-filter-grammar/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/optimaiden/branches/saulius-parse-filter-grammar@107 b9d307a2-75af-4218-822f-dfac11aef269

---
+++ src/filter_tokens.ads
@@ -0,0 +1,51 @@
+package Filter_Tokens is
+
+
+   subtype YYSType is YYSType_Definition.YYSType;
+
+   YYLVal, YYVal : YYSType;
+   type Token is
+        (END_OF_INPUT, ERROR, UNICODE_CHARACTER, FF_TOKEN,
+         LF_TOKEN, CR_TOKEN, HT_TOKEN,
+         VT_TOKEN, AND_TOKEN, NOT_TOKEN,
+         OR_TOKEN, IS_TOKEN, KNOWN_TOKEN,
+         UNKNOWN_TOKEN, CONTAINS_TOKEN, STARTS_TOKEN,
+         ENDS_TOKEN, WITH_TOKEN, LENGTH_TOKEN,
+         HAS_TOKEN, ALL_TOKEN, ONLY_TOKEN,
+         ANY_TOKEN, TRUE_TOKEN, FALSE_TOKEN,
+         '(', ')', '.',
+         ',', ':', '=',
+         'A', 'B', 'C',
+         'D', 'E', 'F',
+         'G', 'H', 'I',
+         'J', 'K', 'L',
+         'M', 'N', 'O',
+         'P', 'Q', 'R',
+         'S', 'T', 'U',
+         'V', 'W', 'X',
+         'Y', 'Z', 'a',
+         'b', 'c', 'd',
+         'e', 'f', 'g',
+         'h', 'i', 'j',
+         'k', 'l', 'm',
+         'n', 'o', 'p',
+         'q', 'r', 's',
+         't', 'u', 'v',
+         'w', 'x', 'y',
+         'z', '_', '"',
+         '\', '!', '#',
+         '$', '%', '&',
+         ''', '*', '+',
+         '-', '/', ';',
+         '<', '>', '?',
+         '@', '[', ']',
+         '^', '`', '{',
+         '|', '}', '~',
+         '0', '1', '2',
+         '3', '4', '5',
+         '6', '7', '8',
+         '9', ' ');
+
+   Syntax_Error : exception;
+
+end Filter_Tokens;
