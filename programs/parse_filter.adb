5de9ac6d6562c36616ee032b47a9275bb8ac4718
saulius <saulius@b9d307a2-75af-4218-822f-dfac11aef269>
2025-09-14 17:21:19 +0000

    Running YYParse in the test driver.
    branches/saulius-parse-filter-grammar/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/optimaiden/branches/saulius-parse-filter-grammar@140 b9d307a2-75af-4218-822f-dfac11aef269

--- programs/parse_filter.adb
+++ programs/parse_filter.adb
@@ -1,12 +1,14 @@
 with Ada.Command_Line;
 with Ada.Text_IO; use Ada.Text_IO;
 
+with Filter_Lexer;
 with Filter; use Filter;
+with YYInput_Definition; use YYInput_Definition;
 
 procedure Parse_Filter is
 
 begin
    
-   null;
+   YYParse;
   
 end Parse_Filter;
