938b4f064cbea7e75023f9986e4b42cd851a036b
saulius <saulius@b9d307a2-75af-4218-822f-dfac11aef269>
2025-09-14 17:53:37 +0000

    Switching debug of the lexer.
    branches/saulius-parse-filter-grammar/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/optimaiden/branches/saulius-parse-filter-grammar@146 b9d307a2-75af-4218-822f-dfac11aef269

--- programs/parse_filter.adb
+++ programs/parse_filter.adb
@@ -2,12 +2,15 @@ with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Filter_Lexer;
+with Filter_Lexer_DFA;
 with Filter; use Filter;
 with YYInput_Definition; use YYInput_Definition;
 
 procedure Parse_Filter is
 
 begin
+   
+   Filter_Lexer_DFA.AFlex_Debug := False;
    
    if Argument_Count = 0 then
       Put_Line ("Parsing default string: """ & Buffer & """");
