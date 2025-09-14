e19c987d32c536980d9854fb35cdaa605d715982
saulius <saulius@b9d307a2-75af-4218-822f-dfac11aef269>
2025-09-14 14:53:57 +0000

    Compuling the new string allocation code.
    branches/saulius-parse-filter-grammar/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/optimaiden/branches/saulius-parse-filter-grammar@124 b9d307a2-75af-4218-822f-dfac11aef269

--- src/yyinput_definition.adb
+++ src/yyinput_definition.adb
@@ -1,13 +1,31 @@
+with Ada.Unchecked_Deallocation;
+
 package body YYInput_Definition is
    
    procedure YY_INPUT
      (
       buf      : out Unbounded_Character_Array;
       result   : out Integer;
-      max_size : in Integer
+      max_size : in  Integer
      ) is
    begin
       null;
+   end;
+   
+   procedure Free is new Ada.Unchecked_Deallocation
+     (String, Access_String);
+   
+   function Buffer return String is
+   begin
+      return Buffer_Ptr.all;
+   end;
+
+   procedure Start_Parsing (S : String) is
+   begin
+      if Buffer_Ptr /= YYInput_Definition.Buffer_Data'Access then
+         Free (Buffer_Ptr);
+      end if;
+      Buffer_Ptr := new String'(S);
    end;
    
 end;
--- src/yyinput_definition.ads
+++ src/yyinput_definition.ads
@@ -2,6 +2,8 @@ with filter_lexer_DFA; use filter_lexer_DFA;
 
 package YYInput_Definition is
    
+   -- Interface to the AFlex generated lexer:
+   
    procedure YY_INPUT
      (
       buf      : out Unbounded_Character_Array;
@@ -9,4 +11,24 @@ package YYInput_Definition is
       max_size : in Integer
      );
    
+   -- Set string S to be the input of the lexer and parser; YY_INPUT
+   --  will read from that string:
+   
+   procedure Start_Parsing (S : String);
+   
+   -- Return thw whole string buffer that was set prviously (or the
+   --  default):
+   
+   function Buffer return String;
+   
+private
+   
+   type Access_String is access all String;
+   
+   Buffer_Data : aliased String := "+0.123";
+   
+   Buffer_Ptr : Access_String := Buffer_Data'Access;
+   
+   Pos : Integer := 0;
+
 end;
